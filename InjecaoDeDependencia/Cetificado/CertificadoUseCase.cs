7676511a7446c0bf7c14d638d862c7ee37216649
Alessandro Augusto <alessandro.augusto@praxio.com.br>
2022-07-28 18:52:32 -0300

    Fazendo ajustes para criar o certificado doda vez que o método for requisitado, mais o mesmo ñ vai criar arquivo fisicamente

--- InjecaoDeDependencia/Cetificado/CertificadoUseCase.cs
+++ InjecaoDeDependencia/Cetificado/CertificadoUseCase.cs
@@ -6,61 +6,41 @@ namespace InjecaoDeDependencia
     public class CertificadoUseCase : ICertificadoUseCase, IDisposable
     {
         private X509Certificate2 _certificate2;
-        private readonly X509Store _store;
         private readonly ICertificadoRepository _certificadoRepository;
 
         public CertificadoUseCase(ICertificadoRepository certificadoRepository)
         {
             _certificadoRepository = certificadoRepository;
-
-            _store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            _store.Open(OpenFlags.ReadWrite);
         }
 
         public void Dispose()
         {
             _certificate2?.Dispose();
-            _store.Dispose();
 
             GC.SuppressFinalize(this);
         }
-
-        public X509Certificate2 this[int id] =>
-            Obter(id);
-
-        private X509Certificate2 this[string serialNumber] =>
-            string.IsNullOrEmpty(serialNumber) ||
-            _store.Certificates.Find(X509FindType.FindBySerialNumber, serialNumber, false) is var certificado && certificado.Count == 0
-                ? null
-                : certificado[0];
 
         public X509Certificate2 Obter(int id)
         {
             var certificadoModel = _certificadoRepository.GetById(id);
 
-            if (this[certificadoModel.SerialNumber] == null)
-            {
-                _certificate2 = new X509Certificate2(
-                    certificadoModel.CaminhoArquivo.Trim(),
-                    certificadoModel.Senha,
-                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            _certificate2 = new X509Certificate2(
+                certificadoModel.CaminhoArquivo.Trim(),
+                certificadoModel.Senha,
+                X509KeyStorageFlags.EphemeralKeySet); // ñ cria o arquivo fisicamente
 
+            if (string.IsNullOrEmpty(certificadoModel.SerialNumber))
                 _certificadoRepository.AtualizarSerialNumber(
                     certificadoModel.Id,
                     _certificate2.SerialNumber);
 
-                _store.Add(_certificate2);
-            }
+            if (_certificate2.NotAfter < DateTime.UtcNow.ToLocalTime())
+                throw new CertificadoCteException(string.Concat(eMensagens.CertificadoExpirado, " - ", _certificate2.NotAfter));
 
-            var retorno = this[certificadoModel.SerialNumber];
+            if (_certificate2.NotBefore > DateTime.UtcNow.ToLocalTime())
+                throw new CertificadoCteException(string.Concat(eMensagens.CertificadoDataInvalida, " - ", _certificate2.NotBefore));
 
-            if (retorno.NotAfter < DateTime.UtcNow.ToLocalTime())
-                throw new CertificadoCteException(string.Concat(eMensagens.CertificadoExpirado, " - ", retorno.NotAfter));
-
-            if (retorno.NotBefore > DateTime.UtcNow.ToLocalTime())
-                throw new CertificadoCteException(string.Concat(eMensagens.CertificadoDataInvalida, " - ", retorno.NotBefore));
-
-            return retorno;
+            return _certificate2;
         }
     }
 }
